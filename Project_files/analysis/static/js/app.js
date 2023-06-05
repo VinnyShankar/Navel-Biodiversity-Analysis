1ca06950f69c9f2fafda1c9aedf84066bc6511c7
VinnyShankar <vinayakshank@gmail.com>
2023-06-05 12:35:51 -0700

    made plotbars dynamic

--- Project_files/analysis/static/js/app.js
+++ Project_files/analysis/static/js/app.js
@@ -1,14 +1,14 @@
 // Use the D3 library to read the .json from the url
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
-async function plotbars()
+async function plotbars(sample_id)
 {
-    let idFilter = "940"
+    let idFilter = sample_id
     let xdata = await d3.json(url).then(x => x.samples.filter(x => x.id == idFilter)[0].sample_values.slice(0,10))
     let ylabel = await d3.json(url).then(x => x.samples.filter(x => x.id == idFilter)[0].otu_ids.slice(0,10).map(x => "OTU " + x.toString()))
     let yhover = await d3.json(url).then(x => x.samples.filter(x => x.id == idFilter)[0].otu_labels.slice(0,10))
-    console.log(xdata)
-    console.log(ylabel)
-    console.log(yhover)
+    // console.log(xdata)
+    // console.log(ylabel)
+    // console.log(yhover)
     xdata.reverse()
     ylabel.reverse()
     yhover.reverse()
@@ -23,4 +23,4 @@ async function plotbars()
     bardata = [trace1]
     Plotly.newPlot("bar",bardata)
 }
-plotbars()
+plotbars("940")
