b13d986d19e5a9347e3164e428282702d0a107e5
VinnyShankar <vinayakshank@gmail.com>
2023-06-13 01:21:46 -0700

    commit was deleted due to rebase, redoing

--- static/js/app.js
+++ static/js/app.js
@@ -10,191 +10,204 @@ d3.json(url)
 .text(x)))
 
 // Dashboard function
-async function plotAll(sample_id)
+function plots(sample_id)
 {
-    // User's selected individual
-    let idFilter = sample_id
-
-    // Data for the selected individual
-    let data = await d3.json(url)
-               .then(x => x.samples
-               .filter(x => x.id == idFilter)[0])
-
-    // Sample Values
-    let sampleValues = data
-                       .sample_values
-
-    // Top 10 OTUs
-    let toptenOTUs = sampleValues
-                     .slice(0,10)
-
-    // OTU ids
-    let otuIds = data
-                 .otu_ids
-    
-    // OTU labels
-    let otuLabels = otuIds
-                    .map(x => "OTU " + x)
-    
-    // Bar plot y labels
-    let ylabel = otuLabels
-                 .slice(0,10)
-
-    // Bar plot hover text (tooltips)
-    let yhover = data
-                 .otu_labels
-                 .slice(0,10)
-    
-    // Demographic Info for the selected indivdual
-    let metaData = await d3.json(url)
-                   .then(x => x.metadata
-                   .filter(x => x.id == sample_id)[0])
-
-    // Reverse arrays for Plotly
-    toptenOTUs.reverse()
-    ylabel.reverse()
-    yhover.reverse()
-
-    // Plotly bar plot
-    let trace1 =
+    d3.json(url)
+    .then(function(x)
     {
-        x:toptenOTUs,
-        y:ylabel,
-        text:yhover,
-        type:"bar",
-        orientation:"h",
-        marker:
-        {
-            color:toptenOTUs,
-            colorscale:"Grayred"
-        }
-    }
+        // Samples for selected id
+        let data = x
+                   .samples
+                   .find(x => x.id == sample_id)
 
-    bardata = [trace1]
-
-    let barlayout = 
-    {
-        title: "Bacterial Abundance per Taxonomic Group"
-    }
-
-    Plotly.newPlot("bar",bardata,barlayout)
-
-    // Clear the Demographic Info - Thanks, Jed
-    let demInfo = d3.select("#sample-metadata").html("")
-
-    // Populate the Demographic Info
-    let newInfo = demInfo.append("text")
-    for (const [x,y] of Object.entries(metaData))
-    {
-        newInfo.append("small")
-               .text(`${x}: ${y}`)
+        console.log(data)
         
-        newInfo.append("br")
-    }
+        // Sample values for selected id
+        let sampleValues = data
+                           .sample_values
 
-    // Plotly bubble chart
-    let trace2 = 
-    {
-        x:otuIds,
-        y:sampleValues,
-        mode: "markers",
-        marker:
-        {
-          size:sampleValues,
-          color:otuIds,
-          colorscale:"Earth"
-        },
-        text:data.otu_labels
-    }
-      
-    let bubbledata = [trace2];
-      
-    let bubblelayout = 
-    {
-    title: "Bacterial Abundance per Taxonomic Group",
-    xaxis:
-    {
-        title:
-        {
-            text:"OTU ID"
-        }
-    },
-    showlegend: false,
-    height: 600,
-    width: 1200
-    }
-    
-    Plotly.newPlot("bubble",bubbledata,bubblelayout)
+        console.log(sampleValues)
+        
+        // Top 10 sample values for selected id
+        let toptenSampleValues = sampleValues
+                             .slice(0,10)
+                             .reverse()
 
-    // Gauge
-    let gaugedata = 
-    [
+        console.log(toptenSampleValues)
+        
+        // OTU Ids for selected id
+        let otuIds = data
+                     .otu_ids
+
+        console.log(otuIds)
+        
+        // Top 10 OTU Ids for selected id
+        let toptenOTUIds = otuIds
+                           .slice(0,10)
+                           .reverse()
+                           .map(x => "OTU "+x)
+
+        console.log(toptenOTUIds)
+        
+        // OTU labels for selected id
+        let otuLabels = data
+                        .otu_labels
+
+        console.log(otuLabels)
+
+        // Top 10 OTU labels for selected id
+        let toptenOTULabels = otuLabels
+                              .slice(0,10)
+                              .reverse()
+        
+        console.log(toptenOTULabels)
+
+        // Metadata for selected id
+        let metaData = x
+                       .metadata
+                       .find(x => x.id == sample_id)
+        
+        console.log(metaData)
+
+        // Plotly bar chart for selected id
+        let bartrace = 
         {
-          type: "indicator",
-          mode: "gauge+number",
-          value: metaData.wfreq,
-          title: 
-          {
-            text:"Navel Washing Frequency <br><sub> Scrubs per Week </sub>",
-            font:{size:24}
-          },
-          gauge: 
-          {
-            axis: 
+            x:toptenSampleValues,
+            y:toptenOTUIds,
+            text:toptenOTULabels,
+            type:"bar",
+            orientation:"h",
+            marker:
             {
-                range:[null,9],
-                tickwidth:1,
-                tickcolor:"darkblue",
-                tickvals:[...Array(10).keys()]
-            },
-            bar: {color:"ff4800"},
-            bgcolor: "white",
-            borderwidth: 2,
-            bordercolor: "gray",
-            steps: 
-            [
-              {range: [0, 1], color: "F3F520"},
-              {range: [1, 2], color: "E2F11D"},
-              {range: [2, 3], color: "D1ED19"},
-              {range: [3, 4], color: "C0E916"},
-              {range: [4, 5], color: "AFE513"},
-              {range: [5, 6], color: "9DE10F"},
-              {range: [6, 7], color: "8CDD0C"},
-              {range: [7, 8], color: "7BD909"},
-              {range: [8, 9], color: "6AD505"},
-            ],
-            threshold: 
-            {
-              line: {color:"red",width:4},
-              thickness: 0.75,
-              value: 9
+                color:toptenSampleValues,
+                colorscale:"Grayred"
             }
-          }
         }
-      ]
-      
-      let gaugelayout = 
-      {
-        width: 400,
-        height: 400,
-        margin: {t:25,r:25,l:25,b:25},
-        font: 
+
+        let bardata = [bartrace]
+
+        let barlayout = 
         {
-            color: "darkblue",
-            family: "Arial" 
+            title:"Top Ten Bacterial Species (OTUs) by Abundance"
         }
-      };
+        
+        Plotly.newPlot("bar",bardata,barlayout)
+
+        // Clear the Demographic Info - Thanks, Jed
+        let demInfo = d3.select("#sample-metadata").html("")
+
+        // Populate the Demographic Info for the selected id
+        let newInfo = demInfo.append("text")
+        for (const [x,y] of Object.entries(metaData))
+        {
+            newInfo.append("small")
+                   .text(`${x}: ${y}`)
+                   .append("br")
+        }
+
+        // Plotly bubble chart for selected id
+        let bubbletrace = 
+        {
+            x:otuIds,
+            y:sampleValues,
+            mode: "markers",
+            marker:
+            {
+                size:sampleValues,
+                color:otuIds,
+                colorscale:"Earth"
+            },
+            text:otuLabels
+        }
       
-      Plotly.newPlot("gauge", gaugedata, gaugelayout)
-      
-      
+        let bubbledata = [bubbletrace];
+        
+        let bubblelayout = 
+        {
+            title: "All Bacterial Species Found in Selected Test Subject (Radius = Abundance)",
+            xaxis:
+            {
+                title:
+                {
+                    text:"OTU ID"
+                }
+            },
+            showlegend: false,
+            height: 600,
+            width: 1200
+        }
+        
+        Plotly.newPlot("bubble",bubbledata,bubblelayout)
+
+        // Plotly gauge for selected id
+        let gaugedata = 
+        [
+            {
+                type: "indicator",
+                mode: "gauge+number",
+                value: metaData.wfreq,
+                title: 
+                {
+                    text:"Navel Washing Frequency <br><sub> Scrubs per Week </sub>",
+                    font:{size:24}
+                },
+                gauge: 
+                {
+                    axis: 
+                    {
+                        range:[null,9],
+                        tickwidth:1,
+                        tickcolor:"darkblue",
+                        tickvals:[...Array(10).keys()]
+                    },
+                    bar: {color:"ff4800"},
+                    bgcolor: "white",
+                    borderwidth: 2,
+                    bordercolor: "gray",
+                    steps: 
+                    [
+                        {range: [0, 1], color: "F3F520"},
+                        {range: [1, 2], color: "E2F11D"},
+                        {range: [2, 3], color: "D1ED19"},
+                        {range: [3, 4], color: "C0E916"},
+                        {range: [4, 5], color: "AFE513"},
+                        {range: [5, 6], color: "9DE10F"},
+                        {range: [6, 7], color: "8CDD0C"},
+                        {range: [7, 8], color: "7BD909"},
+                        {range: [8, 9], color: "6AD505"},
+                    ],
+                    threshold: 
+                    {
+                        line: {color:"red",width:4},
+                        thickness: 0.75,
+                        value: 9
+                    }
+                }
+            }
+        ]
+        
+        let gaugelayout = 
+        {
+            width: 400,
+            height: 400,
+            margin: {t:25,r:25,l:25,b:25},
+            font: 
+            {
+                color: "darkblue",
+                family: "Arial" 
+            }
+        };
+        
+        Plotly.newPlot("gauge", gaugedata, gaugelayout)
+
+    })
 }
 
-// Default dashboard view
-plotAll("940")
+// Default dashboard
+plots("940")
 
 // Repopulate dashboard based on user selection
 function optionChanged(x)
 {
-    plotAll(x)
+    plots(x)
 }
